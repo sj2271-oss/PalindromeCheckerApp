e3586a12c06ec9e580afeba63836cc2d97102592
sj2271-oss <sj2271@srmist.edu.in>
2026-02-25 14:24:40 +0530

    Palindrome

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,41 @@
+/**
+ * ============================================================
+ * MAIN CLASS - PalindroneCheckerApp
+ * ============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ * The goal is to establish a clear startup flow.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println("====================================");
+        System.out.println("Application Version: 1.0");
+        System.out.println("System Initialized Successfully.");
+    }
+}
